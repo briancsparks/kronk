8b5bd4524a1ab69dc60dac850f7b59eac628946b
Brian C Sparks <briancsparks@gmail.com>
2022-01-11 21:15:53 -0800

    Refactoring

--- main.go
+++
@@ -1,9 +0,0 @@
-package xmain
-
-/* Copyright © 2021 Brian C Sparks <briancsparks@gmail.com> -- MIT (see LICENSE file) */
-
-import "github.com/briancsparks/kronk/cmd"
-
-func main() {
-	cmd.Execute()
-}
